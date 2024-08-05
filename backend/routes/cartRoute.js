ab043070c4a5e1bf42841e70b5395811affa37dc
priyanshu kumar <114394901+priyanshu-rj@users.noreply.github.com>
2024-08-05 18:58:26 +0530

    Update cartRoute.js

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -7,7 +7,7 @@ import authMiddleware from "../middleware/auth.js";
 
 
 const cartRouter = express.Router();
-
+//route addtocar remove cart and get cart here PK
 cartRouter.post("/add",authMiddleware,addToCart)
 cartRouter.post("/remove",authMiddleware,removeFromCart)
 cartRouter.post("/get",authMiddleware,getCart)
